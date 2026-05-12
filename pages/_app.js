76e7a831d24d0efe9348b25fdea07fb8101d7732
Sylvester Van der vlies <sylvestervandervlies@Sylvesters-Mac-mini.local>
2026-05-12 21:45:05 +0200

    Add dynamic favicon updates based on language selection
    - Update browser tab favicon when language changes
    - Update apple-touch-icon for iOS home screen shortcuts
    - Icon now reflects the currently selected language
    - Uses useEffect to watch for language changes and updates DOM dynamically

--- pages/_app.js
+++ pages/_app.js
@@ -11,10 +11,41 @@ import { DarkModeProvider } from '../context/DarkModeContext';
 import Sidebar from '../components/Sidebar';
 import MobileNav from '../components/MobileNav';
 import { useRouter } from 'next/router';
+import { useLanguage } from '../context/LanguageContext';
+import { useEffect } from 'react';
 
-function MyApp({ Component, pageProps }) {
+function AppContent({ Component, pageProps }) {
   const router = useRouter();
+  const { language } = useLanguage();
   const isHomePage = router.pathname === '/' || router.pathname === '/index';
+
+  // Dynamically update favicon based on language selection
+  useEffect(() => {
+    const iconMap = {
+      en: '/icon-e-192.png',
+      nl: '/icon-n-192.png',
+      ru: '/icon-r-192.png',
+      tr: '/icon-t-192.png',
+    };
+    
+    const iconPath = iconMap[language] || '/icon-e-192.png';
+    
+    // Update favicon in browser tab
+    let faviconLink = document.querySelector("link[rel='icon'][type='image/png']");
+    if (!faviconLink) {
+      faviconLink = document.createElement('link');
+      faviconLink.rel = 'icon';
+      faviconLink.type = 'image/png';
+      document.head.appendChild(faviconLink);
+    }
+    faviconLink.href = iconPath;
+    
+    // Update Apple touch icon (for iOS home screen)
+    let appleTouchIcon = document.querySelector("link[rel='apple-touch-icon']");
+    if (appleTouchIcon) {
+      appleTouchIcon.href = iconPath;
+    }
+  }, [language]);
 
   return (
     <DarkModeProvider>
@@ -30,6 +61,16 @@ function MyApp({ Component, pageProps }) {
             <Component {...pageProps} />
           </FinancialProvider>
         </CurrencyProvider>
+      </LanguageProvider>
+    </DarkModeProvider>
+  );
+}
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <DarkModeProvider>
+      <LanguageProvider>
+        <AppContent Component={Component} pageProps={pageProps} />
       </LanguageProvider>
     </DarkModeProvider>
   );
